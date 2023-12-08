60648ac82414ff0425f137f0cb24e51ce91fb2ee
NIMCOO <nimco77001@gmail.com>
2023-12-08 17:41:04 +0000

    finished

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Good Luck 💪🏾
+ const plusHashCount = myString =>{
+    let count = 0;
+    const plusHashCount=("###+")
+ }
+ console.log(plusHashCount)
+ 
+ 
+ plusHashCount("###+") 
+
+
+ const PlusHashCount = myString =>{
+    let count = 0;
+    const plusHashCount=("###+")
+ }
+ console.log(PlusHashCount)
+ 
+
+
+plusHashCount("##+++#") 
+ 
+
